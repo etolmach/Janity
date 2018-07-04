64a493d2ee0934b0efed08017f65df3f89510cd1
etolmach <evgeniy.tolmach@gmail.com>
2018-07-04 14:59:54 +0200

    Added filters `lessThan` and `lessOrEqualTo`

--- src/main/java/com/etolmach/stream/Filter.java
+++ src/main/java/com/etolmach/stream/Filter.java
@@ -59,4 +59,58 @@ public class Filter {
         return notEqualsTo(value.doubleValue());
     }
 
+    // =====================================================================
+
+    public static IntPredicate lessThan(int value) {
+        return i -> i < value;
+    }
+
+    public static LongPredicate lessThan(long value) {
+        return i -> i < value;
+    }
+
+    public static DoublePredicate lessThan(double value) {
+        return i -> i < value;
+    }
+
+    public static IntPredicate lessThan(Integer value) {
+        return lessThan(value.intValue());
+    }
+
+    public static LongPredicate lessThan(Long value) {
+        return lessThan(value.longValue());
+    }
+
+    public static DoublePredicate lessThan(Double value) {
+        return lessThan(value.doubleValue());
+    }
+
+    // =====================================================================
+
+    public static IntPredicate lessOrEqualTo(int value) {
+        return i -> i <= value;
+    }
+
+    public static LongPredicate lessOrEqualTo(long value) {
+        return i -> i <= value;
+    }
+
+    public static DoublePredicate lessOrEqualTo(double value) {
+        return i -> i <= value;
+    }
+
+    public static IntPredicate lessOrEqualTo(Integer value) {
+        return lessOrEqualTo(value.intValue());
+    }
+
+    public static LongPredicate lessOrEqualTo(Long value) {
+        return lessOrEqualTo(value.longValue());
+    }
+
+    public static DoublePredicate lessOrEqualTo(Double value) {
+        return lessOrEqualTo(value.doubleValue());
+    }
+
+
+
 }
