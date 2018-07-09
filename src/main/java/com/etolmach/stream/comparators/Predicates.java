d963f1fac64a366313c08e1c8e40792b2f2b7367
etolmach <evgeniy.tolmach@gmail.com>
2018-07-09 22:37:40 +0200

    Added implementation of `not` operator and reformatted `trueFor` methods

--- src/main/java/com/etolmach/stream/comparators/Predicates.java
+++ src/main/java/com/etolmach/stream/comparators/Predicates.java
@@ -7,24 +7,60 @@ import java.util.function.*;
  */
 public class Predicates {
 
-    public static Predicate<IntPredicate> testing(int value) {
+    public static IntPredicate not(IntPredicate predicate) {
+        return predicate.negate();
+    }
+
+    public static LongPredicate not(LongPredicate predicate) {
+        return predicate.negate();
+    }
+
+    public static DoublePredicate not(DoublePredicate predicate) {
+        return predicate.negate();
+    }
+
+    public static <T> Predicate<T> not(Predicate<T> predicate) {
+        return predicate.negate();
+    }
+
+    public static Predicate<IntPredicate> trueFor(int value) {
         return predicate -> predicate.test(value);
     }
 
-    public static Predicate<LongPredicate> testing(long value) {
+    public static Predicate<LongPredicate> trueFor(long value) {
         return predicate -> predicate.test(value);
     }
 
-    public static Predicate<DoublePredicate> testing(double value) {
+    public static Predicate<DoublePredicate> trueFor(double value) {
         return predicate -> predicate.test(value);
     }
 
-    public static <T> Predicate<Predicate<T>> testing(T value) {
+    public static <T> Predicate<Predicate<T>> trueFor(T value) {
         return predicate -> predicate.test(value);
     }
 
-    public static <T1, T2> Predicate<BiPredicate<T1, T2>> testing(T1 value1, T2 value2) {
+    public static Predicate<IntPredicate> trueFor(IntSupplier supplier) {
+        return predicate -> predicate.test(supplier.getAsInt());
+    }
+
+    public static Predicate<LongPredicate> trueFor(LongSupplier supplier) {
+        return predicate -> predicate.test(supplier.getAsLong());
+    }
+
+    public static Predicate<DoublePredicate> trueFor(DoubleSupplier supplier) {
+        return predicate -> predicate.test(supplier.getAsDouble());
+    }
+
+    public static <T> Predicate<Predicate<T>> trueFor(Supplier<T> supplier) {
+        return predicate -> predicate.test(supplier.get());
+    }
+
+    public static <T1, T2> Predicate<BiPredicate<T1, T2>> trueFor(T1 value1, T2 value2) {
         return predicate -> predicate.test(value1, value2);
+    }
+
+    public static <T1, T2> Predicate<BiPredicate<T1, T2>> trueFor(Supplier<T1> supplier1, Supplier<T2> supplier2) {
+        return predicate -> predicate.test(supplier1.get(), supplier2.get());
     }
 
 }
