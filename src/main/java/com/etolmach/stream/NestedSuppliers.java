37ebd201c8a5bda7730317c19c0032f5b639c735
etolmach <evgeniy.tolmach@gmail.com>
2018-07-09 22:03:53 +0200

    Added nested suppliers

---
+++ src/main/java/com/etolmach/stream/NestedSuppliers.java
@@ -0,0 +1,54 @@
+package com.etolmach.stream;
+
+import java.util.function.*;
+
+/**
+ * @author Evgeniy Tolmach
+ */
+public class NestedSuppliers {
+
+    public static Supplier<Boolean> boxed(BooleanSupplier supplier) {
+        return supplier::getAsBoolean;
+    }
+
+    public static Supplier<Integer> boxed(IntSupplier supplier) {
+        return supplier::getAsInt;
+    }
+
+    public static Supplier<Long> boxed(LongSupplier supplier) {
+        return supplier::getAsLong;
+    }
+
+    public static Supplier<Double> boxed(DoubleSupplier supplier) {
+        return supplier::getAsDouble;
+    }
+
+    public static BooleanSupplier unboxedBoolean(Supplier<Boolean> supplier) {
+        return supplier.get()::booleanValue;
+    }
+
+    public static IntSupplier unboxedInt(Supplier<Number> supplier) {
+        return supplier.get()::intValue;
+    }
+
+    public static LongSupplier unboxedLong(Supplier<Number> supplier) {
+        return supplier.get()::longValue;
+    }
+
+    public static DoubleSupplier unboxedDouble(Supplier<Number> supplier) {
+        return supplier.get()::doubleValue;
+    }
+
+    public static Supplier<Integer> asInt(Supplier<Number> supplier) {
+        return boxed(unboxedInt(supplier));
+    }
+
+    public static Supplier<Long> asLong(Supplier<Number> supplier) {
+        return boxed(unboxedLong(supplier));
+    }
+
+    public static Supplier<Double> asDouble(Supplier<Number> supplier) {
+        return boxed(unboxedDouble(supplier));
+    }
+
+}
